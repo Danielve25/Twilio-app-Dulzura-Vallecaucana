6ea1700b66a4d03cd5649d4c22d419e247150c8c
Danielve25 <118493652+Danielve25@users.noreply.github.com>
2025-02-18 22:38:30 -0500

    Delete routes directory

--- routes/messageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { sendMessage } = require('../controllers/messageController'); // Importar el controlador de mensajes
-
-const router = express.Router();
-
-// Ruta POST para enviar mensajes de WhatsApp
-router.post('/send-message', sendMessage);
-
-module.exports = router;
